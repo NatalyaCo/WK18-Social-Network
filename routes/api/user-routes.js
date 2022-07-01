0c955a7dd8eee3fa0033ce0c9b7d1918d7ce11c1
Natalya Colburn <greatqueenbee@gmail.com>
2022-06-30 21:14:11 -0600

    added the old-school way as commented out section to ask my tutor to user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -18,3 +18,22 @@ router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
+
+// router
+//   .route('/')
+//   .get(getAllUser)
+//   .post(createUser);
+
+
+// router
+//   .route('/:id')
+//   .get(getUserById)
+//   .put(updateUser)
+//   .delete(deleteUser);
+
+// router
+//   .route('/:userId/friends/:friendId')
+//   .post(addFriend)
+//   .delete(deleteFriend);
+
+//is this a better way?
