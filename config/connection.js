911d73a8e1c7a680fae6dbbdacd9add01ac156fe
Natalya Colburn <greatqueenbee@gmail.com>
2022-06-30 22:00:45 -0600

    formatted documents to look better

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
+    process.env.MONGODB_URI || "mongodb://localhost:27017/social-network", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
 );
 
 mongoose.set("debug", true);
--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(routes);
 
 db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
 });
